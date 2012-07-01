f30ef17998d58dd67aa17e902cf5567efd3767a1
Dennis Kehrig <mailgithub@denniskehrig.de>
2012-07-01 21:05:31 +0200

    Goodbye, explicit CodeMirror reference

--- InlineColorPicker.js
+++ InlineColorPicker.js
@@ -45,9 +45,8 @@ define(function (require, exports, module) {
 	InlineColorPicker.prototype.$wrapperDiv = null;
 
 	InlineColorPicker.prototype.setColor = function (hex) {
-		var cm = this.editor._codeMirror;
 		var end = { line: this.pos.line, ch: this.pos.ch + this.color.length };
-		cm.replaceRange(hex, this.pos, end);
+		this.editor.document.replaceRange(hex, this.pos, end);
 		this.color = hex;
 	};
 
