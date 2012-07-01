4385cf2095cc054274d24c63a419c668688711b3
Dennis Kehrig <mailgithub@denniskehrig.de>
2012-07-01 20:57:21 +0200

    Close the color picker when a mousedown was performed anywhere outside it, but not inside it (The bug was not taking bubbling into account, this fixes it AND extends the click-to-close area to more than just the inline editor)

--- InlineColorPicker.js
+++ InlineColorPicker.js
@@ -66,8 +66,18 @@ define(function (require, exports, module) {
 			}
 		});
 
-        this.$htmlContent.append(this.$wrapperDiv);
-        this.$htmlContent.click(this.close.bind(this));
+		this.$htmlContent.append(this.$wrapperDiv);
+		$(document).on('mousedown.InlineColorPicker', this.onDocumentClick.bind(this));
+	};
+
+	// Close the color picker when clicking somewhere outside of it
+	InlineColorPicker.prototype.onDocumentClick = function (event) {
+		// Ignore clicks inside the actual color picker's area
+		if ($(event.target).closest('div.colorpicker').length) { return; }
+		// Remove this event handler
+		$(document).off('mousedown.InlineColorPicker');
+		// Close the inline color picker
+		this.close();
 	};
 
 	InlineColorPicker.prototype.close = function () {
